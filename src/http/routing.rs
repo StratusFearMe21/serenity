4786973de91acf00a2697c0545f423469d1959c0
Isaac Mills <rooster0055@protonmail.com>
2025-05-11 20:31:38 -0600

    Add some endpoints

--- src/http/routing.rs
+++ src/http/routing.rs
@@ -354,6 +354,14 @@ routes! ('a, {
     api!("/guilds/{}/threads/active", guild_id),
     Some(RatelimitingKind::PathAndId(guild_id.into()));
 
+    HeadlessSessions,
+    api!("/users/@me/headless-sessions"),
+    Some(RatelimitingKind::Path);
+
+    DeleteHeadlessSessions,
+    api!("/users/@me/headless-sessions/delete"),
+    Some(RatelimitingKind::Path);
+
     Guilds,
     api!("/guilds"),
     Some(RatelimitingKind::Path);
